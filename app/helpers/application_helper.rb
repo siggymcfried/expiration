be2c08de047e94aa71e607af8b938125efc3d6fd
Michael Siegfried <michael@procore.com>
2015-05-09 11:22:04 -0700

    Addding font awesome icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def icon_for(icon_name)
+    Icon.new(icon_name: icon_name).render
+  end
 end
---
+++ app/models/icon.rb
@@ -0,0 +1,27 @@
+class Icon
+  include ActionView::Helpers::TagHelper
+  include ActionView::Context 
+
+  def initialize(icon_name:)
+    @icon_name = icon_name
+  end
+
+  def render
+    content_tag(:i, nil, class: icon_class)
+  end
+
+  private
+  attr_reader :icon_name
+
+  def icon_class
+    'fa ' + icon_classes.fetch(icon_name)
+  end
+
+  def icon_classes
+    {
+      eaten: 'fa-cutlery',
+      edit:  'fa-edit',
+      trash: 'fa-trash-o'
+    }
+  end
+end
