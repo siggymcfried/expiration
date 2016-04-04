2a1f402fa4a15a96a0c9cf643fa84be346bc232c
Michael Siegfried <michael@procore.com>
2016-04-03 18:33:15 -0700

    Adding ability to edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to food_items_path
+    else
+      render :edit
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
     post :finish_eating
     post :throw_out
   end
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit, :update]
 end
