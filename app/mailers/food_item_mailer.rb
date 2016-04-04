acda4ad5065213447c141fc7f238d9c730f3d26b
Michael Siegfried <michael@procore.com>
2016-04-03 18:04:54 -0700

    Only care about uneaten untrashed food for email

--- app/mailers/food_item_mailer.rb
+++ app/mailers/food_item_mailer.rb
@@ -3,8 +3,8 @@ class FoodItemMailer < ApplicationMailer
 
   def food_status_email(user)
     @user = user
-    @expired_items = user.food_items.expired
-    @expiring_items = user.food_items.expiring_by(1.week.from_now).expiring_after(Date.today)
+    @expired_items = user.food_items.expired.not_eaten.not_trashed.ordered
+    @expiring_items = user.food_items.expiring_by(1.week.from_now).expiring_after(Date.today).not_eaten.not_trashed.ordered
 
     mail(to: @user.email, subject: 'Foods')
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true, format: /@/
   validates :password, length: { in: 6..72 }, confirmation: true
 
-  scope :with_expired_or_expiring_food, -> { joins(:food_items).merge(FoodItem.expiring_by(1.week.from_now)) }
+  scope :with_expired_or_expiring_food, -> { joins(:food_items).merge(FoodItem.expiring_by(1.week.from_now).not_eaten.not_trashed) }
 
   has_many :food_items
 
