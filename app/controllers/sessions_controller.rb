1852963e622d3fcdf23251ef45178b7a4bd46a55
Michael Siegfried <michael@procore.com>
2017-02-02 15:13:48 -0800

    Remove annoying message.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,6 @@ class SessionsController < ApplicationController
     authorized_user = User.authenticate(username_or_email: params[:username_or_email], password: params[:login_password])
     if authorized_user
       session[:user_id] = authorized_user.id
-      flash[:notice] = "Wow Welcome again, you logged in as #{authorized_user.username}"
       redirect_to food_items_path
     else
       flash[:notice] = "Invalid Username or Password"
