05a065e7b4b16b90ac28b414a4608e1c0d2356d3
Michael Siegfried <michael@procore.com>
2017-02-02 15:51:46 -0800

    Remove references to password and username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ActiveRecord::Base
-  validates :username, presence: true, uniqueness: true, length: { in: 3..20 }
   validates :email, presence: true, uniqueness: true, format: /@/
-  validates :password, length: { in: 6..72 }, confirmation: true
 
   scope :with_expired_or_expiring_food, -> { joins(:food_items).merge(FoodItem.expiring_by(1.week.from_now).not_eaten.not_trashed) }
 
