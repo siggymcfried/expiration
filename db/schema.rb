e89ae3a6655ac4870e94ebac4c29f3c93aa60fd4
Michael Siegfried <michael@procore.com>
2017-06-06 23:08:50 -0700

    Revert db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170202232753) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,4 +30,5 @@ ActiveRecord::Schema.define(version: 20170202232753) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
