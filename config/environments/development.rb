91564f86589577d211ca55ebece8d26532f0de1d
Michael Siegfried <michael@procore.com>
2017-06-06 22:25:45 -0700

    Enforce hash rockets.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,6 @@ Rails.application.configure do
   config.assets.digest = true
   config.assets.raise_runtime_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
