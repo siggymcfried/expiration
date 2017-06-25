7447b5541f97e4e53e3fe6dc2f2053ac7e534a2a
Michael Siegfried <michael@procore.com>
2017-06-24 20:07:07 -0700

    Add explicit permission requests.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,6 +5,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
     access_type: :offline,
     name: :google,
-    prompt: :select_account
+    prompt: :select_account,
+    scope: %w[contacts plus.login plus.me email profile]
   }
 end
