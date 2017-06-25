5d32d3de10c354d0069c7079df17f9e23c12e1f1
Michael Siegfried <michael@procore.com>
2017-06-25 13:59:22 -0700

    Fix regex.
    Need to allow a string that is not exactly one character with an @
    symbol...

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  validates :email, presence: true, uniqueness: true, format: /\A@\z/
+  validates :email, presence: true, uniqueness: true, format: /\A.*@.*\z/
   validates :provider, :uid, :first_name, :last_name, :token, :oauth_expires_at, presence: true
 
   scope :with_expired_or_expiring_food, -> { joins(:foods).merge(Food.expiring_by(1.week.from_now).not_eaten.not_trashed) }
