396456cddd5e0f27a1d338485d03aab870212069
Michael Siegfried <michael@procore.com>
2017-06-25 15:12:51 -0700

    Add food recovery specs.

--- spec/features/change_food_eaten_status_spec.rb
+++ spec/features/change_food_eaten_status_spec.rb
@@ -18,6 +18,26 @@ RSpec.feature "Change food's status" do
     expect(page).to have_text('Yummy food')
   end
 
+  scenario 'user recovers eaten food' do
+    food = FactoryGirl.create(:food, :eaten, name: 'Misplaced Food')
+
+    visit root_path
+    mock_auth_hash(user: food.user)
+    click_link 'Sign in with Google'
+
+    click_link 'filter_by_status_eaten'
+    expect(page).to have_text('Misplaced Food')
+
+    click_link "edit_food_#{food.id}"
+    fill_in 'food_eaten_on', with: ''
+    click_button 'Update Food'
+    click_link 'filter_by_status_eaten'
+    expect(page).to_not have_text('Misplaced Food')
+
+    click_link 'filter_by_status_expiring'
+    expect(page).to have_text('Misplaced Food')
+  end
+
   scenario 'user trashes food' do
     food = FactoryGirl.create(:food, name: 'Old food')
 
@@ -31,5 +51,25 @@ RSpec.feature "Change food's status" do
 
     click_link 'filter_by_status_trashed'
     expect(page).to have_text('Old food')
+  end
+
+  scenario 'user recovers trashed food' do
+    food = FactoryGirl.create(:food, :trashed, name: 'Questionable Food')
+
+    visit root_path
+    mock_auth_hash(user: food.user)
+    click_link 'Sign in with Google'
+
+    click_link 'filter_by_status_trashed'
+    expect(page).to have_text('Questionable Food')
+
+    click_link "edit_food_#{food.id}"
+    fill_in 'food_trashed_on', with: ''
+    click_button 'Update Food'
+    click_link 'filter_by_status_trashed'
+    expect(page).to_not have_text('Questionable Food')
+
+    click_link 'filter_by_status_expiring'
+    expect(page).to have_text('Questionable Food')
   end
 end
