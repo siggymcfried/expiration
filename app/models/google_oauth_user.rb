fd93b7a2ea46a39f7383a342f7aca93cc11fe066
Michael Siegfried <michael@procore.com>
2017-06-24 19:15:48 -0700

    Adding google oauth user poro.
    This helps create a user from a hash.

---
+++ app/models/google_oauth_user.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class GoogleOauthUser
+  def initialize(oauth_hash)
+    @oauth_hash = oauth_hash
+    @user = User.first_or_initialize(uid: uid)
+  end
+
+  def update_or_create
+    user.update!(params)
+    user
+  end
+
+  private
+  attr_reader :oauth_hash, :user
+
+  def uid
+    oauth_hash.fetch(:uid)
+  end
+
+  # rubocop:disable Metrics/AbcSize
+  def params
+    {
+      provider: oauth_hash[:provider],
+      email: oauth_hash[:info][:email],
+      first_name: oauth_hash[:info][:first_name],
+      last_name: oauth_hash[:info][:last_name],
+      token: oauth_hash[:credentials][:token],
+      refresh_token: oauth_hash[:credentials][:refresh_token],
+      oauth_expires_at: oauth_hash[:credentials][:expires_at]
+    }
+  end
+end
