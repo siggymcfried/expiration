d33f1eca65db31f09cc3c6a33357d9b00a7558e3
Michael Siegfried <michael@procore.com>
2017-12-13 10:37:37 -0800

    Fix method length violation.

--- spec/support/omniauth_macros.rb
+++ spec/support/omniauth_macros.rb
@@ -1,21 +1,29 @@
 # frozen_string_literal: true
 
 module OmniAuthMacros
-  # rubocop:disable Metrics/MethodLength
   def mock_auth_hash(user: FactoryGirl.create(:user), expires_at: 1.day.from_now)
     OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new(
       provider: :google,
       uid: user.uid,
-      info: {
-        email: user.email,
-        first_name: user.first_name,
-        last_name: user.last_name
-      },
-      credentials: {
-        token: 'abcdefg12345',
-        refresh_token: '12345abcdefg',
-        expires_at: expires_at
-      }
+      info: user_info_for(user: user),
+      credentials: credentials(expiring_at: expires_at)
     )
+  end
+
+  private
+  def user_info_for(user:)
+    {
+      email: user.email,
+      first_name: user.first_name,
+      last_name: user.last_name
+    }
+  end
+
+  def credentials(expiring_at:)
+    {
+      token: 'abcdefg12345',
+      refresh_token: '12345abcdefg',
+      expires_at: expiring_at
+    }
   end
 end
