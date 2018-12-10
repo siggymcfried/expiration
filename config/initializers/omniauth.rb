aa90e9eda4c21372547762fa55426ceef6773243
Michael Siegfried <michael@procore.com>
2018-12-09 20:19:37 -0800

    Enforce table alignment. Autocorrect.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,9 +4,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # rubocop:disable Style/BracesAroundHashParameters
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
     access_type: :offline,
-    name: :google,
-    prompt: :select_account,
-    scope: %w[plus.login plus.me email profile]
+    name:        :google,
+    prompt:      :select_account,
+    scope:       %w[plus.login plus.me email profile]
   }
   # rubocop:enable Style/BracesAroundHashParameters
 end
--- spec/support/omniauth_macros.rb
+++ spec/support/omniauth_macros.rb
@@ -3,9 +3,9 @@
 module OmniAuthMacros
   def mock_auth_hash(user: FactoryGirl.create(:user), expires_at: 1.day.from_now)
     OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new(
-      provider: :google,
-      uid: user.uid,
-      info: user_info_for(user: user),
+      provider:    :google,
+      uid:         user.uid,
+      info:        user_info_for(user: user),
       credentials: credentials(expiring_at: expires_at)
     )
   end
@@ -13,17 +13,17 @@ module OmniAuthMacros
   private
   def user_info_for(user:)
     {
-      email: user.email,
+      email:      user.email,
       first_name: user.first_name,
-      last_name: user.last_name
+      last_name:  user.last_name
     }
   end
 
   def credentials(expiring_at:)
     {
-      token: 'abcdefg12345',
+      token:         'abcdefg12345',
       refresh_token: '12345abcdefg',
-      expires_at: expiring_at
+      expires_at:    expiring_at
     }
   end
 end
