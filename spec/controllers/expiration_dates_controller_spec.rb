2d70e3abbdeee4322d5d108f1ff2e07ca899fb59
Michael Siegfried <michael@procore.com>
2019-06-20 12:25:42 -0700

    Fix intermittent spec.
    ```
    irb(main):005:0> (4.months.ago.to_date - 6.months.ago.to_date).days
    => 62/1 days
    irb(main):006:0> 2.months.ago.to_date
    => Sat, 20 Apr 2019
    irb(main):007:0> 62.days.ago.to_date
    => Fri, 19 Apr 2019
    ```
    
    As shown above, with months that are varying lengths, the number of days
    in two random months may not be the same as the number of days from two
    months ago.

--- spec/controllers/expiration_dates_controller_spec.rb
+++ spec/controllers/expiration_dates_controller_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe ExpirationDatesController do
 
   describe 'GET show' do
     context 'when the food exists' do
-      before { FactoryBot.create(:food, name: 'strawberries', created_at: 6.months.ago, expiration: 4.month.ago) }
+      before { FactoryBot.create(:food, name: 'strawberries', created_at: 6.days.ago, expiration: 4.days.ago) }
 
       it 'calculates the date' do
         get :show, params: { id: 'strawberries' }, session: { user_id: user.id }, format: :json
 
-        expect(response.body).to eq({ expires_on: 2.months.from_now.to_date }.to_json)
+        expect(response.body).to eq({ expires_on: 2.days.from_now.to_date }.to_json)
       end
     end
 
